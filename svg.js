44475cc314b7ff61890da710e96cdfd7cadf9289
FelicityPictures <felicitypictures@yahoo.com>
2016-03-05 18:04:38 -0500

    changing directions

--- svg.js
+++ svg.js
@@ -1,14 +1,37 @@
 var c = document.getElementById("canvas");
+var currentX = 200;
+var currentY = 200;
+var direction;
+/*
+  direction 1 = up (38)
+  direction 2 = right (39)
+  direction 3 = down (40)
+  direction 4 = left (37)
+*/
+
 var play = function play(e){
     console.log(e.keyCode);
-    if (e.keyCode != 38) {
-	return;
+        var r = document.createElementNS("http://www.w3.org/2000/svg","rect");
+        r.setAttribute("x",currentX);
+        r.setAttribute("y",currentY);
+        r.setAttribute("width",20);
+        r.setAttribute("height",20);
+        c.appendChild(r);
+    if (e.keyCode == 38) {
+        direction=1;
+    }else{
+        if(e.keyCode == 39){
+            direction = 2;
+        }else{
+            if(e.keyCode == 40){
+                direction = 3;
+            }else{
+                if(e.keyCode == 37){
+                    direction = 4;
+                }
+            }
+        }
     }
-    var r = document.createElementNS("http://www.w3.org/2000/svg","rect");
-    r.setAttribute("x",200);
-    r.setAttribute("y",200);
-    r.setAttribute("width",20);
-    r.setAttribute("height",20);
-    c.appendChild(r);
+    console.log("direction: " + direction);
 }
 document.onkeydown = play;
